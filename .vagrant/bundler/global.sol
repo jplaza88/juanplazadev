3adfe3c02d1b2c9766d7da48d6705ab3c6e4bba4
Juan Plaza <jplaza@sincerususa.com>
2021-11-26 23:41:09 -0500

    Initial commit

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-docker-compose",["= 1.5.1"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"3bfbe5d17f2fe65fd9b188776586fcc0bfbae739bd9d9453db40b40cac12fe5a","vagrant_version":"2.2.18"}
